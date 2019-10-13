ce0ecdc4f476424564922e8fc8fd6df651d7a728
yaoqilin <1392675219@qq.com>
2019-10-13 09:39:26 +0800

    添加识字界面

--- pages/font/font.js
+++ pages/font/font.js
@@ -5,13 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    array: [{
+        font: '大',
+        words: '大树，大楼'
+      },
+      {
+        font: '小',
+        words: '小草，小鱼'
+      }, {
+        font: '高',
+        words:'高山,高兴'
+      }, {
+        font: '低',
+        words:'高低，低温'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     wx.request({
       url: 'http://api.jisuapi.com',
@@ -23,15 +37,17 @@ Page({
       header: {
         'content-type': 'application/x-www-form-urlencoded'
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res);
-        that.setData({ textdata: res.data });
+        that.setData({
+          textdata: res.data
+        });
         console.log("项目传来的数据" + res.data.haha);
       },
-      fail: function () {
+      fail: function() {
         // fail
       },
-      complete: function () {
+      complete: function() {
         console.log("d");
       }
     })
@@ -40,49 +56,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
